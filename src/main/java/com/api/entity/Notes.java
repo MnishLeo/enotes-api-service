5f6e29655ff99193b0d5a5577dc2bc7bbc11f496
Manish <manish@outlook.com>
2026-01-09 21:59:34 +0530

    copy-paste-notes

--- src/main/java/com/api/entity/Notes.java
+++ src/main/java/com/api/entity/Notes.java
@@ -14,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,6 +24,7 @@ import lombok.Setter;
 @AllArgsConstructor 
 @Getter
 @Setter
+@Builder
 @EntityListeners(AuditingEntityListener.class)
 public class Notes extends BaseModel {
 	@Id
